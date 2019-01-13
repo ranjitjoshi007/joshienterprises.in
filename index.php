1748063b8c3e9daca497ea57cea2a6431c24a4b6
Ranjit <joshiranjit1@gmail.com>
2019-01-13 19:59:01 +0530

    r->Add Title to index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <title>Material Design Bootstrap</title>
+  <title>Joshi Enterprises</title>
   <!-- Font Awesome -->
   <link rel="stylesheet" href="CSS/all.css">
   <!-- Bootstrap core CSS -->
